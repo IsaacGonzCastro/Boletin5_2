31df7ca892767d79993fc216db6f16738d444629
igonzalezcastro <igonzalezcastro@atanasoff21.danielcastelao.org>
2016-02-04 12:15:27 +0100

    boletin5_2

---
+++ src/boletin5_2/Boletin5_2.java
@@ -0,0 +1,14 @@
+package boletin5_2;
+
+/**
+ *
+ * @author igonzalezcastro
+ */
+public class Boletin5_2 {
+
+    public static void main(String[] args) {
+        Satelite obx = new Satelite(3.5,10,4);
+        obx.verPosicion();
+    }
+
+}
---
+++ src/boletin5_2/Satelite.java
@@ -0,0 +1,30 @@
+package boletin5_2;
+
+/**
+ *
+ * @author igonzalezcastro
+ */
+public class Satelite {
+
+    private double meridiano;
+    private double paralelo;
+    private double distanciaTerra;
+
+    public Satelite() {
+
+        meridiano = paralelo = distanciaTerra = 0;
+    }
+
+    public Satelite(double m, double p, double d) {
+
+        meridiano = m;
+        paralelo = p;
+        distanciaTerra = d;
+
+    }
+
+    public void verPosicion() {
+
+        System.out.println("o satelite atopase no paralelo : " + paralelo + " ,meridiano : " + meridiano + " e a unha distancia da terra é :" + distanciaTerra);
+    }
+}
